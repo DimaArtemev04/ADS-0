253e2182b0139c8b5d1a0f34f06b6baf87024f84
DimaArtemev04 <160415382+DimaArtemev04@users.noreply.github.com>
2024-03-02 13:14:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+	int max=0,i=1;
+	if (a == b)
+		max = a;
+	else if(a>b)
+		while (i<= b)
+		{
+			if (a % i == 0 && b % i == 0)
+				max = i;
+			i++;
+		}
+	else
+		while (i<=a)
+		{
+			if (a % i == 0 && b % i == 0)
+				max = i;
+			i++;
+		}
+	return max;
 }
